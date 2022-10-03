5b2b60277e98423ec9356e066019b4622718f185
callum buttery <callumbuttery@icloud.com>
2022-10-03 21:43:55 +0100

    Feat: get api data + loading stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>Hello world</div>
+  <main v-if="!loading">Hello world</main>
+  <main v-else class="flex flex-col align-center justify center text-center">
+    <img :src="loadingImage" class="w-35 mx-auto mt-10 mb-4" />
+    <div class="text-grey-500 text-3xl mt-6 mb-10">Fetching Covid-19 data</div>
+  </main>
 </template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: null,
+      stats: {},
+      countries: {},
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      try {
+        const data = await fetch("https://api.covid19api.com/summary");
+        return await data.json();
+      } catch (error) {
+        throw new Error("API fell over: ", error);
+      }
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    if (!data) {
+      throw new Error("No data provided");
+    }
+
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+    console.log("data: ", data);
+  },
+};
+</script>
