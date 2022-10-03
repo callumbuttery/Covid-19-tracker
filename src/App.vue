9efe47daf5e56d9d21c66454e608523506d9ad05
callum buttery <callumbuttery@icloud.com>
2022-10-03 21:10:45 +0100

    Feat; first files + header file

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header/>
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  }
+}
+</script>
