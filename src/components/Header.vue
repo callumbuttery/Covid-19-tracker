b1556eea7d6a629f670178a91326fbc365d4d341
Callum Buttery <callumbuttery@icloud.com>
2022-10-04 23:22:33 +0100

    Fix: styling changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl font-bold md:text-5xl pb-5">Covid-19 tracker <i class="fa fa-virus pl-2"></i></div>
+    <div class="text-3xl font-bold md:text-5xl pb-5">Covid-19 Stats Tracker <i class="fa fa-virus pl-2"></i></div>
   </header>
 </template>
 
