3932ca69f997a7d04c956a2d805c4f7c1258b901
Callum Buttery <callumbuttery@icloud.com>
2022-10-07 09:12:44 +0100

    fix: country dropdown now populating

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -2,7 +2,7 @@
     <div>
         <select v-model="selected" class="form-select mb-10 mt-10 block w-6/12 mx-auto border p-4 ">
             <option value="0">Select country</option>
-            <option v-if="country in countries" :value="country.ID">{{country.Country}}</option>
+            <option v-for="country in countries" :value="country.ID">{{country.Country}}</option>
         </select>
     </div>
 </template>
